b72bfaf8e2e4e26bc909ee520163038583186185
Rick S <rssedivec@gmail.com>
2026-03-06 14:25:12 -0500

    chore: add production config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Environment-specific configuration
+// DO NOT merge this file between branches
+const SUPABASE_URL      = 'https://plgegklqtdjxeglvyjte.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsZ2Vna2xxdGRqeGVnbHZ5anRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NjEwNzgsImV4cCI6MjA4NzQzNzA3OH0.fqWECTPl5FJab7wNXP8UHbc2BCve_kETcSWrwQ7cQpU';
