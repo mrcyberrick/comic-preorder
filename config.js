5d485793970f7392d6ec2a1b5dd522dba6596c06
Rick S <rssedivec@gmail.com>
2026-06-15 14:58:51 -0400

    config(staging): add FOUNDING_TENANT to staging config.js (F71, 5.3 S2)

--- config.js
+++ config.js
@@ -2,3 +2,8 @@
 // DO NOT merge this file between branches
 const SUPABASE_URL      = 'https://puoaiyezsreowpwxzxhj.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_1jCe57nM0bKPBBZiMbo27Q_3Ztm1gb2';
+window.FOUNDING_TENANT = {
+  id: '72e29f67-39f7-42bc-a4d5-d6f992f9d790',
+  slug: 'raysandjudys',
+  display_name: "Ray & Judy's Book Stop",
+};
